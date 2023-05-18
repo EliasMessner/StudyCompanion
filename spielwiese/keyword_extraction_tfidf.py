db1c0094141d0ddbc2951fec9f09dd102101851f
Elias Messner <em83zewo@studserv.uni-leipzig.de>
2023-05-18 20:57:10 +0200

    Added extract_keywords_from_pdf

--- spielwiese/keyword_extraction_tfidf.py
+++ spielwiese/keyword_extraction_tfidf.py
@@ -1,14 +1,21 @@
+from langchain.document_loaders import PyPDFLoader
 from sklearn.feature_extraction.text import TfidfVectorizer
 
 from text_preprocessing import get_stopwords, clean
 
 
-def get_top_n_keywords(text, n=5, stop_words=None):
+def extract_keywords_from_pdf(path_to_pdf, stop_words=None, n=5):
+    if stop_words is None:
+        stop_words = get_stopwords("german", "english")
+    return extract_keywords_from_text(pdf_to_str(path_to_pdf), stop_words=stop_words, n=n)
+
+
+def extract_keywords_from_text(text, n=5, stop_words=None):
     if stop_words is None:
         stop_words = get_stopwords("english", "german")
     text_cleaned = clean(text, stop_words)
     kw_scores = get_keyword_scores(text_cleaned)
-    return [item[0] for item in sorted(kw_scores.items(), key=lambda item: item[1], reverse=True)][:n]
+    return sorted(kw_scores.items(), key=lambda item: item[1], reverse=True)[:n]
 
 
 def get_keyword_scores(text):
@@ -26,3 +33,9 @@ def get_keyword_scores(text):
     for col in tfidf_matrix.nonzero()[1]:
         keyword_scores[feature_names[col]] = tfidf_matrix[0, col]
     return keyword_scores
+
+
+def pdf_to_str(pdf_path):
+    loader = PyPDFLoader(pdf_path)
+    documents = loader.load()
+    return ' '.join(doc.page_content for doc in documents)
