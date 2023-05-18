33017a865fb64e1058c5c3411e19f1c8ff36dc96
Elias Messner <em83zewo@studserv.uni-leipzig.de>
2023-05-18 13:45:59 +0200

    Split file kw-extract into kw-extract and preprocessing. Added link to tf-idf

--- spielwiese/keyword_extraction.py
+++ spielwiese/keyword_extraction.py
@@ -1,7 +1,6 @@
-import string
-
-import nltk
 from sklearn.feature_extraction.text import TfidfVectorizer
+
+from text_preprocessing import get_stopwords, preprocess_text
 
 
 def get_top_n_keywords(text, n, stop_words=None):
@@ -10,20 +9,6 @@ def get_top_n_keywords(text, n, stop_words=None):
     tokens = preprocess_text(text, stop_words)
     kw_scores = get_keyword_scores(tokens)
     return [item[0] for item in sorted(kw_scores.items(), key=lambda item: item[1], reverse=True)][:n]
-
-
-def get_stopwords(*languages):
-    return [sw for lang in languages for sw in nltk.corpus.stopwords.words(lang)]
-
-
-def preprocess_text(text, stop_words):
-    # tokenize
-    tokens = nltk.tokenize.word_tokenize(text)
-    # remove punctuation
-    tokens = [token for token in tokens if token not in string.punctuation]
-    # remove stopwords and make lowercase
-    tokens = [token.lower() for token in tokens if token.lower() not in stop_words]
-    return tokens
 
 
 def get_keyword_scores(tokens):
