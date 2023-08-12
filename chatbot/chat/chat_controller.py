82ad33eb913f69956ebb44563e6bd3b55ce1618f
EliasMessner <elias151019@gmail.com>
2023-08-12 17:05:11 +0200

    finished summarized strategy

--- chatbot/chat/chat_controller.py
+++ chatbot/chat/chat_controller.py
@@ -1,9 +1,9 @@
-from chat.prompt_strategy import RetrievalQA
+from chat.prompt_strategy import RetrievalQA, SummarizedConversationPromptStrategy
 
 
 class ChatController:
     def __init__(self):
-        self.prompt_strategy = RetrievalQA()
+        self.prompt_strategy = SummarizedConversationPromptStrategy()  # TODO select in front-end
 
     def on_new_user_message(self, messages: list):
         return self.prompt_strategy.execute(messages=messages)
--- chatbot/chat/prompt_template.py
+++ chatbot/chat/prompt_template.py
@@ -1,4 +1,4 @@
-def fill_simple_retrieval_QA_template(context, question):
+def fill_simple_retrieval_qa_template(context, question):
     return f"""
     Use the context to answer the question. Context and question are delimited by XML tags. If the context is not sufficient, say so and don't make up an answer.
 
@@ -9,4 +9,25 @@ def fill_simple_retrieval_QA_template(context, question):
     <question>
     {question}
     </question>
-    """ 
+    """
+
+
+def fill_summarization_template(chat_history, follow_up_question):
+    return f"""
+    Given the following conversation and a follow-up question, return a summary of the conversation history that\
+    includes all relevant context to the question and rephrase the follow up question to be a standalone question.
+    Chat History: {chat_history}
+    Follow-Up Input: {follow_up_question}
+    Your answer should follow the following format (replace only the text between the XML tags and keep the XML tags):
+    \"\"\"
+    Use the following pieces of context to answer the users question.
+    If you don't know the answer, just say that you don't know, don't try to make up an answer.
+    ----------------
+    <context>
+    *Relevant chat history excerpt as context here*
+    </context>
+    <question>
+    *Rephrased question here*
+    </question>
+    \"\"\"
+    """
