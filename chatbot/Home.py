b6762c0828f3a52971c07382062220f0c1af808f
Felix Vogel <nx27rixe@studserv.uni-leipzig.de>
2023-08-12 13:11:05 +0200

    Style changes

--- chatbot/Home.py
+++ chatbot/Home.py
@@ -1,4 +1,8 @@
 import streamlit as st
 
-st.title("StudyCompanion")
-st.write('Hello world!')
+st.markdown("<h1 style='text-align: center;'>StudyCompanion</h1>", unsafe_allow_html=True)
+st.divider()
+
+st.subheader('Instructions')
+st.markdown("1. Upload your PDF files [here](/Upload_Slides).")
+st.markdown("2. Ask the Chat Bot about your slides at [here](/Chat).")
