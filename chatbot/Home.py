7cf93ca626d27d6eb4fe4cd6415fb38a2f8c517c
Felix Vogel <vogel.felix.hasslau@gmx.de>
2023-08-23 21:00:01 +0200

    fix typo

--- chatbot/Home.py
+++ chatbot/Home.py
@@ -1,10 +1,11 @@
 import streamlit as st
 
-st.markdown("<h1 style='text-align: center;'>StudyCompanion</h1>", unsafe_allow_html=True)
+st.markdown("<h1 style='text-align: center;'>StudyCompanion</h1>",
+            unsafe_allow_html=True)
 st.divider()
 
 st.subheader('Instructions')
 
 st.markdown("1. Upload your PDF files [here](/Upload_Slides).")
 
-st.markdown("2. Ask the Chat Bot about your slides at [here](/Chat).")
+st.markdown("2. Ask the Chat Bot about your slides [here](/Chat).")
